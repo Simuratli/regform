ab91889702263a6abdde0ed92d160f256c3ed031
Admin <admin@MacBook-Pro-Admin-2.local>
2021-01-08 11:49:03 +0200

    refactoring

--- src/services/services.js
+++ src/services/services.js
@@ -1,42 +1,39 @@
-import authentication from '../b2c';
-import axios from 'axios';
+import authentication from "../b2c";
+import axios from "axios";
 
 const { accessToken } = authentication.getAccessToken();
 
-const BASE_URL = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/api/v1/'
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api/v1/"
     : `https://my-uds-systems-website-development.azurewebsites.net/api/v1/`;
 
 // const BASE_URL = process.env.NODE_ENV === 'development'
 //     ? 'http://localhost:5000/api/v1/'
 //     : `https://my.uds.systems/api/v1/`;
 
-
-
-// https://my-uds-systems-website-development.azurewebsites.net/api/v1/add-ons/uds-interface-switcher/file-resources/uds-portal/uds-interface-switcher_1_0_0_4.zip
-
 const httpClient = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        Authorization: `Bearer ${accessToken}`
-    }
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
 });
 
-const resolve = res => res;
+const resolve = (res) => res;
 
-const reject = history => err => {
-    const { status } = err.response;
-    if (status === 401) {
-        authentication.signOut();
-    } else if (status === 404) {
-        history.push('/404')
-    } else {
-        return Promise.reject(err);
-    }
-}
+const reject = (history) => (err) => {
+  const { status } = err.response;
+  if (status === 401) {
+    authentication.signOut();
+  } else if (status === 404) {
+    history.push("/404");
+  } else {
+    return Promise.reject(err);
+  }
+};
 
 function initializeHttpClientSettings(history) {
-    httpClient.interceptors.response.use(resolve, reject(history))
+  httpClient.interceptors.response.use(resolve, reject(history));
 }
 
-export { httpClient, initializeHttpClientSettings }
+export { httpClient, initializeHttpClientSettings };
