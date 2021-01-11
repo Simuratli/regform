64c76e55d7985f6d835a3c819088fd3bf0677261
Admin <admin@MacBook-Pro-Admin-2.local>
2021-01-11 11:50:11 +0200

    add loading

---
+++ ~/UDS-Portal-API/Src/UDS.Portal.Api/build/precache-manifest.895f4a2c3d718037f4134e5f5da29f28.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce430d56eeefbf316a75e1cf63767e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41f89d8cbfdb8d750dad",
+    "url": "/static/css/5.302941e4.chunk.css"
+  },
+  {
+    "revision": "450f1efa1432e12bf8db",
+    "url": "/static/css/main.91fc8944.chunk.css"
+  },
+  {
+    "revision": "5f9b16079ca7fbfd0125",
+    "url": "/static/js/0.a089ad39.chunk.js"
+  },
+  {
+    "revision": "6060f0c138111c5fc8a7",
+    "url": "/static/js/3.a6b43280.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/3.a6b43280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58dd384b156b55d5cc0",
+    "url": "/static/js/4.f75177e4.chunk.js"
+  },
+  {
+    "revision": "ac6e7cfb2f3b1ce4d81b21955f04e509",
+    "url": "/static/js/4.f75177e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f89d8cbfdb8d750dad",
+    "url": "/static/js/5.b3dbd1bb.chunk.js"
+  },
+  {
+    "revision": "48d4cb2c9a35dcf497e4",
+    "url": "/static/js/6.0d0620a5.chunk.js"
+  },
+  {
+    "revision": "450f1efa1432e12bf8db",
+    "url": "/static/js/main.7570c149.chunk.js"
+  },
+  {
+    "revision": "cb8bfca8f6cecd35c246",
+    "url": "/static/js/runtime-main.210a643c.js"
+  },
+  {
+    "revision": "6b9f4bbb310f1490270fbc84d4db0680",
+    "url": "/static/media/SegoeUI-BoldItalic.6b9f4bbb.eot"
+  },
+  {
+    "revision": "7c08f51390e4717400ceb8a874bbc5a0",
+    "url": "/static/media/SegoeUI-BoldItalic.7c08f513.woff"
+  },
+  {
+    "revision": "c040af578cb85c8f4c944a08a3f5640f",
+    "url": "/static/media/SegoeUI-BoldItalic.c040af57.ttf"
+  },
+  {
+    "revision": "02b73ca1085e2d21021bd618f54ed4c6",
+    "url": "/static/media/SegoeUI-Light.02b73ca1.woff"
+  },
+  {
+    "revision": "b34ee12d90d0e7c0b64a4e10bc3cd75d",
+    "url": "/static/media/SegoeUI-Light.b34ee12d.ttf"
+  },
+  {
+    "revision": "db415e98e36924d8f493b2fffbe5f55d",
+    "url": "/static/media/SegoeUI-Light.db415e98.eot"
+  },
+  {
+    "revision": "1b14ce9a2a4021a0946936d2dc0344e9",
+    "url": "/static/media/SegoeUI-SemiBold.1b14ce9a.woff"
+  },
+  {
+    "revision": "310d75469691cb2a4402176b6ddbbeef",
+    "url": "/static/media/SegoeUI-SemiBold.310d7546.ttf"
+  },
+  {
+    "revision": "cb5a10bce6b123948755cdfc2b4f4afe",
+    "url": "/static/media/SegoeUI-SemiBold.cb5a10bc.eot"
+  },
+  {
+    "revision": "03bcc7ef261b8ccbc4a434bb78b386b6",
+    "url": "/static/media/SegoeUI.03bcc7ef.eot"
+  },
+  {
+    "revision": "463298714bb46fea5c4cb58f569767a1",
+    "url": "/static/media/SegoeUI.46329871.ttf"
+  },
+  {
+    "revision": "ee10411f11ca61338f5bcaebefbdf700",
+    "url": "/static/media/SegoeUI.ee10411f.woff"
+  },
+  {
+    "revision": "6e94a2ca74d9f03e7b8916e103a45a72",
+    "url": "/static/media/addon-white-square-with-border.6e94a2ca.svg"
+  },
+  {
+    "revision": "15b7b02a2263c53598372a611273bef9",
+    "url": "/static/media/addon_full_page_header_bg.15b7b02a.svg"
+  },
+  {
+    "revision": "c8e93bfe190d37caba1fa1543c1421c4",
+    "url": "/static/media/addons_card_bg.c8e93bfe.svg"
+  },
+  {
+    "revision": "422d6c22c401a7f60115b43c5074078e",
+    "url": "/static/media/bottom-lines.422d6c22.svg"
+  },
+  {
+    "revision": "efb3b105e031aee3917f581fadb8bd05",
+    "url": "/static/media/footer_logo.efb3b105.svg"
+  },
+  {
+    "revision": "efb3b105e031aee3917f581fadb8bd05",
+    "url": "/static/media/header_logo.efb3b105.svg"
+  },
+  {
+    "revision": "074160e34b97e6107bc38a3b05192da4",
+    "url": "/static/media/mail.074160e3.svg"
+  },
+  {
+    "revision": "c9ec68d171e75ee6b95bb8dfac889e28",
+    "url": "/static/media/orange_element.c9ec68d1.svg"
+  },
+  {
+    "revision": "ee6e3125902b9d0075a62e7ae501bc26",
+    "url": "/static/media/point.ee6e3125.svg"
+  },
+  {
+    "revision": "2996d38a2b2b2380d578209823bebf24",
+    "url": "/static/media/skype.2996d38a.svg"
+  },
+  {
+    "revision": "1e303a547490b802ffc5975d93265040",
+    "url": "/static/media/top-lines.1e303a54.svg"
+  }
+]);
