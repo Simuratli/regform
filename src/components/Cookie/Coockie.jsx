1ad3f958818fc147e0eebcab507095a0e5708570
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2021-01-25 14:57:47 +0200

    Redesign mobile pop-up

--- src/components/Cookie/Coockie.jsx
+++ src/components/Cookie/Coockie.jsx
@@ -12,7 +12,7 @@ const Cookie = () => {
                            declineButtonText="Decline"
                            buttonClasses={"acceptButton"}
                            declineButtonClasses={"declineButton"}
-                           cookieName="myAwesomeCookieName2"
+                           cookieName="Coockie"
                            containerClasses={"cookieContainer"}
                            contentClasses={"cookieContent"}
                            buttonWrapperClasses={"buttons"}
@@ -21,7 +21,7 @@ const Cookie = () => {
             >
                 We use cookies on this site to enhance your user experience.
                 By continuing to use the service, you agree to our use of cookies as described in the{" "}
-                <Link to={'/privacy'} style={{color: "#F46036", textDecoration: "underline"}}>Privacy Policy.</Link>
+                <Link to={'/privacy'} style={{color: "#FF8F3E", textDecoration: "underline"}}>Privacy Policy.</Link>
             </CookieConsent>
 
         </>
--- src/components/Modal/ModalMobileNotification.jsx
+++ src/components/Modal/ModalMobileNotification.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import "../../scss/modal/modal.scss";
+import "../../scss/modal/modalMobileNotification.scss";
+import warning from "../../assets/images/warning-icon.svg";
 
 import Modal from "./Modal";
 
-const ModalMobileNotification = ({ active, setActive }) => {
-  return (
-    <Modal active={active} setActive={setActive}>
-      <p>
-        The present mobile version provides general information about UDS
-        services and products.
-        <span className={"boldText"}>
-          {" "}
-          We recommend using the desktop version to download and use any UDS
-          product.
-        </span>
-      </p>
-      <button className={"agreeButton"} onClick={() => setActive(false)}>
-        OK
-      </button>
-    </Modal>
-  );
+const ModalMobileNotification = ({active, setActive}) => {
+    return (
+        <Modal active={active} setActive={setActive}>
+            <img src={warning} alt={"info"} style={{margin: "auto"}}/>
+            <h5 className={"mobileModalTitle boldText"}>For your information</h5>
+            <p>
+                The present mobile version provides general information about UDS
+                services and products.
+            </p>
+            <p className={"boldText"}>
+                We recommend using the desktop version to download and use any UDS product.
+            </p>
+            <button className={"agreeButton"} onClick={() => setActive(false)}>
+            </button>
+        </Modal>
+    );
 };
 export default ModalMobileNotification;
