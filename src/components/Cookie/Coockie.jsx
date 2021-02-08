e5a087c034fb2f08c095dd3242b278a367d04ccb
Admin <admin@MacBook-Pro-Admin-2.local>
2021-02-08 17:23:12 +0200

    fix filter zIndex

--- src/components/Cookie/Coockie.jsx
+++ src/components/Cookie/Coockie.jsx
@@ -2,32 +2,42 @@ import React from "react";
 import CookieConsent from "react-cookie-consent";
 import "../../scss/base.scss";
 
-
 const Cookie = () => {
-    return(
-        <>
-            <CookieConsent enableDeclineButton flipButtons
-                           location="bottom"
-                           buttonText="Ok"
-                           declineButtonText="none"
-                           buttonClasses={"acceptButton"}
-                           declineButtonClasses={"declineButton"}
-                           cookieName="Cookie"
-                           containerClasses={"cookieContainer"}
-                           contentClasses={"cookieContent"}
-                           buttonWrapperClasses={"buttons"}
-                           buttonStyle={{color: "#4e503b", fontSize: "13px"}}
-                           expires={150}
-            >
-                <p>We use cookies to ensure you an improved user experience and services.
-                    <br/>By using our website, you consent to cookies.</p>
-               <p> To find out more — read our {" "}
-                <a href={'https://uds.systems/privacy-policy'} target={"_blank"}
-                   style={{color: "#FF8F3E", textDecoration: "underline"}}>Privacy Policy.</a></p>
-            </CookieConsent>
-
-        </>
-    )
-}
+  return (
+    <>
+      <CookieConsent
+        enableDeclineButton
+        flipButtons
+        location="bottom"
+        buttonText="Ok"
+        declineButtonText="none"
+        buttonClasses={"acceptButton"}
+        declineButtonClasses={"declineButton"}
+        cookieName="Cookie"
+        containerClasses={"cookieContainer"}
+        contentClasses={"cookieContent"}
+        buttonWrapperClasses={"buttons"}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={0}
+      >
+        <p>
+          We use cookies to ensure you an improved user experience and services.
+          <br />
+          By using our website, you consent to cookies.
+        </p>
+        <p>
+          {" "}
+          To find out more — read our{" "}
+          <a
+            href={"https://uds.systems/privacy-policy"}
+            target={"_blank"}
+            style={{ color: "#FF8F3E", textDecoration: "underline" }}
+          >
+            Privacy Policy.
+          </a>
+        </p>
+      </CookieConsent>
+    </>
+  );
+};
 export default Cookie;
-
