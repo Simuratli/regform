c0eef006e8f77fde5586e3922eb8b9df208f8d46
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2021-02-25 18:10:00 +0200

    Errors fix

--- src/store/reducers/addonReducer/actions/fullAddonPageAction.js
+++ src/store/reducers/addonReducer/actions/fullAddonPageAction.js
@@ -1,34 +1,40 @@
-import { httpClient } from "../../../../services/services";
-import { setError, setIsLoading } from "../../appReducer/actions/appAction";
-import { FULL_ADDON_PAGE_DATA_LOADED } from "../types";
+import {httpClient} from "../../../../services/services";
+import {setError, setIsLoading} from "../../appReducer/actions/appAction";
+import {FULL_ADDON_PAGE_DATA_LOADED} from "../types";
+import get from "lodash/get";
 
 export const getFullAddonPage = (slug) => {
-  return (dispatch) => {
-    dispatch(setIsLoading(true));
+    return (dispatch) => {
+        dispatch(setIsLoading(true));
 
-    // const sessionStorage = JSON.parse(localStorage.getItem(slug));
-    // if (sessionStorage) {
-    //   return dispatch({ type: DATA_LOADED, payload: sessionStorage });
-    // }
+        // const sessionStorage = JSON.parse(localStorage.getItem(slug));
+        // if (sessionStorage) {
+        //   return dispatch({ type: DATA_LOADED, payload: sessionStorage });
+        // }
 
-    httpClient
-      .get(`add-ons/${slug}`)
-      .then((res) => {
-        // sessionStorage.setItem(slug, JSON.stringify(res.data));
+        httpClient
+            .get(`add-ons/${slug}`)
+            .then((res) => {
+                // sessionStorage.setItem(slug, JSON.stringify(res.data));
 
-        dispatch({ type: FULL_ADDON_PAGE_DATA_LOADED, payload: res.data });
+                dispatch({type: FULL_ADDON_PAGE_DATA_LOADED, payload: res.data});
 
-        dispatch(setIsLoading(false));
-      })
-      .catch((err) => {
-        dispatch(setIsLoading(false));
-        dispatch(setError(err.response));
-      });
-  };
+                dispatch(setIsLoading(false));
+            })
+            .catch((err) => {
+                dispatch(setError(err));
+                localStorage.setItem(
+                    "error",
+                    JSON.stringify(err)
+                );
+
+                dispatch(setIsLoading(false));
+            });
+    };
 };
 
 export const resetFullAddonPage = () => {
-  return (dispatch) => {
-    dispatch({ type: FULL_ADDON_PAGE_DATA_LOADED, payload: {} });
-  };
+    return (dispatch) => {
+        dispatch({type: FULL_ADDON_PAGE_DATA_LOADED, payload: {}});
+    };
 };
