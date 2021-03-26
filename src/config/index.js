753fef7b8cdab27e6523e93842965fdb6fb35066
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2021-03-26 13:59:15 +0200

    Refactor authentication file and delete no need files

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-import { stepsConfig } from './step';
-import { jobsTableBaseConfig, entitiesTableConfig } from './table';
-
-export {
-  jobsTableBaseConfig,
-  stepsConfig,
-  entitiesTableConfig,
-}
