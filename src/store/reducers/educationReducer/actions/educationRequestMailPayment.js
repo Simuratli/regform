6658cc53f68e02c7dd33127004c663e320e0d39b
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2021-06-28 22:16:59 +0300

    Change post payment data in action

--- src/store/reducers/educationReducer/actions/educationRequestMailPayment.js
+++ src/store/reducers/educationReducer/actions/educationRequestMailPayment.js
@@ -6,13 +6,7 @@ export const SET_EDUCATION_REQUEST_PAYMENT = "SET_EDUCATION_REQUEST_PAYMENT";
 export const educationRequestMailPayment = ({paymentData}) => {
     return (dispatch) => {
         httpClient
-            .post(`/payment/request-mail-payment`, {
-                "firstName": "string",
-                "lastName": "string",
-                "email": "string",
-                "courseSlug": "string",
-                "paymentMessage": "string"
-            })
+            .post(`/payment/request-mail-payment`, paymentData)
             .then((res) => {
                 dispatch({ type: SET_EDUCATION_REQUEST_PAYMENT, payload: res.data });
                 dispatch(setIsLoading(false));
