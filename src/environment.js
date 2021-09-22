e148cd950f11ffef34d9f24c3b05166a2d377898
bastare <noobfrizz1@gmail.com>
2021-09-22 20:07:43 +0300

    Rafactoring

--- src/environment.js
+++ src/environment.js
@@ -1,25 +1,29 @@
+const PRODUCTION_ENVIRONMENT = 'production',
+    DEVELOPMENT_ENVIRONMENT = 'development',
+    STAGE_ENVIRONMENT = 'stage';
+
 const environment = (() => {
     switch (process.env.REACT_APP_ENV) {
-        case "development":
+        case DEVELOPMENT_ENVIRONMENT:
             return {
                 BASE_URI: 'http://localhost:5000/api/v1/',
                 REDIRECT_URI: 'http://localhost:6420'
             };
 
-        case "production":
+        case PRODUCTION_ENVIRONMENT:
             return {
                 BASE_URI: 'https://my.uds.systems/api/v1/',
                 REDIRECT_URI: 'https://my.uds.systems'
             };
 
-        case "stage":
+        case STAGE_ENVIRONMENT:
             return {
                 BASE_URI: 'https://my-uds-systems-website-development.azurewebsites.net/api/v1/',
                 REDIRECT_URI: 'https://my-uds-systems-website-development.azurewebsites.net/'
             };
 
         default:
-            throw new Error("No valid environment");
+            throw new Error(`Not valid environment: ${process.env.REACT_APP_ENV}`);
     }
 })();
 
