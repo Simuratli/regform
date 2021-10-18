37a9bb3a87cccf83070d79e64960c22b3a3dbf07
Anastasiya Muzychenko <an.muzychenko@uds.systems>
2021-10-18 12:17:25 +0300

    Change whatsaap button position

--- src/components/ViewsComponents/WhatsappButton.jsx
+++ src/components/ViewsComponents/WhatsappButton.jsx
@@ -4,7 +4,7 @@ import whatsappIcon from "../../assets/images/whatsapp-icon.svg";
 function WhatsappButton() {
 
     return (
-        <div style={{position: "fixed", right: "73px", bottom: "97px", zIndex: "998"}}>
+        <div style={{position: "fixed", right: "43px", bottom: "43px", zIndex: "998"}}>
             <a target={"_blank"} href={"https://api.whatsapp.com/send/?phone=+380953839341"}>
                 <img style={{width: 45}} src={whatsappIcon} alt="Whatsapp"/>
             </a>
